d926c1171461faa4b921ad68bc4828b0fa58091f
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2023-11-01 09:55:23 -0600

    Se agrego otro modelo al archivo prediction

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@ import joblib
 
 def predict(data, model):
     if model == 'Random Forest Classificator':
-        model = joblib.load('random_forest_model.pkl')
+        model = joblib.load('random_forest_model_regressor.pkl')
     
     pipeline = joblib.load("full_pipeline.pkl")
     data = pipeline.transform(data)
